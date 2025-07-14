40b2454860becd81bd18067bab912c7e4392600a
ASN <amansinghnishad0808@gmail.com>
2025-07-14 14:15:29 +0530

    about page started

---
+++ AboutPage/src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import Navbar from "./component/section/Navbar";
+import SectionHeader from "./component/section/sectionHeader";
+import OurStorySection from "./component/section/ourMissionSection";
+import MissionVisionSection from "./component/section/missionVisionSection";
+import CoreValueSection from "./component/section/coreValueSection";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Navbar />
+      <SectionHeader />
+      <OurStorySection />
+      <MissionVisionSection />
+      <CoreValueSection />
+      <main></main>
+    </div>
+  );
+}
+
+export default App;
