7d56122b3ec8c686627886b9799e72ff0315c20e
Anand Varshney <anandvarshney2005@gmail.com>
2025-07-16 15:00:15 +0530

    Added new About Page code

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import About from './pages/About.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <About />
+    </div>
+  );
+}
+
+export default App;
