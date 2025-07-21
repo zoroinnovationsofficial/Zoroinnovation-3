1ee63baf5be30dfecd308627c8dce2ee353d5a34
BhashiVarshney <bhashi7467@gmail.com>
2025-07-21 19:26:58 +0530

    Add Career page code

---
+++ src/data/heroMatrics.js
@@ -0,0 +1,8 @@
+const heroMetrics = [
+  { value: '500+', label: 'Projects Completed' },
+  { value: '98%', label: 'Client Satisfaction' },
+  { value: '25+', label: 'Industries Served' },
+  { value: '15', label: 'Years Experience' },
+];
+
+export default heroMetrics;
