8f282f4f29df6b73f6b6e6924a189f2161c03c1b
RajathRavikumar <rajathravikumar2205@gmail.com>
2025-07-28 22:30:51 +0530

    Update PartnerEcosystem.js

--- src/components/homePage/PartnerEcosystem.js
+++ src/components/homePage/PartnerEcosystem.js
@@ -9,12 +9,12 @@ function PartnerEcosystem() {
       </p>
 
       <div className="flex flex-wrap justify-center gap-8 px-4">
-        <img src="/images/Background.svg" alt="Partner 1" className="h-16 w-auto" />
-        <img src="/images/Background-1.svg" alt="Partner 2" className="h-16 w-auto" />
-        <img src="/images/Background-2.svg" alt="Partner 3" className="h-16 w-auto" />
-        <img src="/images/Background-3.svg" alt="Partner 4" className="h-16 w-auto" />
-        <img src="/images/Background-4.svg" alt="Partner 5" className="h-16 w-auto" />
-        <img src="/images/Background-5.svg" alt="Partner 6" className="h-16 w-auto" />
+        <img src="/Homeimages/Background.svg" alt="Partner 1" className="h-16 w-auto" />
+        <img src="/Homeimages/Background-1.svg" alt="Partner 2" className="h-16 w-auto" />
+        <img src="/Homeimages/Background-2.svg" alt="Partner 3" className="h-16 w-auto" />
+        <img src="/Homeimages/Background-3.svg" alt="Partner 4" className="h-16 w-auto" />
+        <img src="/Homeimages/Background-4.svg" alt="Partner 5" className="h-16 w-auto" />
+        <img src="/Homeimages/Background-5.svg" alt="Partner 6" className="h-16 w-auto" />
       </div>
     </section>
   );
