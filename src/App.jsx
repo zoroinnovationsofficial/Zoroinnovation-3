d0963d2a764e21e69a9d607f7e0aee6915edb66b
RajathRavikumar <rajathravikumar2205@gmail.com>
2025-07-28 22:32:37 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./pages/About.jsx";
 import VerifyIDPage from "./pages/VerifyIDPage";
 import ContactPage from "./pages/ContactPage";
-import ServicePage from "./pages/servicePage";// ✅ Add this line
+import ServicePage from "./pages/servicePage";
+import HomePage from "./pages/homePage";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
@@ -16,6 +17,7 @@ function App() {
         <Route path="/verify" element={<VerifyIDPage />} />
         <Route path="/contact" element={<ContactPage />} /> 
         <Route path="/service" element={<ServicePage />} />
+        <Route path="/home" element={<HomePage />} />
       </Routes>
       <Footer />
     </Router>
