4daf440aa725991c870752971770137d6084485d
Rajath Ravikuamar <rajathravikumar2205@gmail.com>
2025-09-05 00:49:58 +0530

    backend

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -1,18 +1,4 @@
-import app from '../app.js';
-import serverless from 'serverless-http';
-import dotenv from 'dotenv';
-import connectDB from '../db/db.js';
+import app from "../index.js";
 
-dotenv.config({ path: './.env' });
-
-let isConnected = false;
-
-async function handler(req, res) {
-  if (!isConnected) {
-    await connectDB();
-    isConnected = true;
-  }
-  return serverless(app)(req, res);
-}
-
-export default handler;
+// ✅ Vercel serverless function entry point
+export default app;
