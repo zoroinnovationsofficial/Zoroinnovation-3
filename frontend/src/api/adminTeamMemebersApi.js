fd9a3b8e726a9c88997e45d6c0fb31b920c186fa
zoroinnovationsofficial <zoroinnovationsofficial@gmail.com>
2025-09-14 23:25:33 +0530

    Update adminTeamMemebersApi.js

--- frontend/src/api/adminTeamMemebersApi.js
+++ frontend/src/api/adminTeamMemebersApi.js
@@ -1,7 +1,7 @@
 // src/api/teamMemberApi.js
 import axios from "axios";
 
-const API_BASE = "https://zoroinnovations-backend.vercel.app";
+const API_BASE = process.env.REACT_APP_API_URL;
 
 const apiClient = axios.create({
   baseURL: API_BASE,
