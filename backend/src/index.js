04d8fbf584d27b1887579ff4e9faf33c456a9965
TalariSrinivas <165267278+TalariSrinivas@users.noreply.github.com>
2025-09-16 02:04:25 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,7 @@ import mongoose from 'mongoose';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import fileUpload from 'express-fileupload';
+
 // Get current directory for ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
