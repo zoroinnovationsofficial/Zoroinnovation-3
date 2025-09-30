8f3fbcaa54513cd4ded9bcdf07d17c1f4a5a33e7
zoroinnovationsofficial <zoroinnovationsofficial@gmail.com>
2025-09-30 10:12:03 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -60,7 +60,7 @@ app.use(
       }
     },
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
   }),
 );
